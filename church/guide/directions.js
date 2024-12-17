51b7262266fb0a569440679298eea2b234c41e21
hyunmin1128 <kilhyunmin1128@naver.com>
2024-12-18 01:42:52 +0900

    kakao map API 시도, 실패, 이유 못찾음

--- church/guide/directions.js
+++ church/guide/directions.js
@@ -1,23 +1,29 @@
 window.onload = function () {
-  if (typeof kakao === "undefined") {
-      console.error("Kakao API 로드 실패");
-      return;
-  }
-  var mapContainer = document.getElementById('map'); // 지도를 표시할 div
-  var mapOption = {
-      center: new kakao.maps.LatLng(37.486803, 126.937059), // 지도의 중심 좌표
-      level: 3 // 확대 레벨
-  };
-  var map = new kakao.maps.Map(mapContainer, mapOption); // 지도 생성
+    console.log("Kakao 객체 상태:", window.kakao);
 
-  var markerPosition = new kakao.maps.LatLng(37.486803, 126.937059);
-  var marker = new kakao.maps.Marker({
-      position: markerPosition
-  });
-  marker.setMap(map);
+    if (window.kakao && window.kakao.maps) {
+        console.log("Kakao Maps API 로드 성공!");
+    } else {
+        console.error("Kakao Maps API 로드 실패. API 키 또는 도메인 등록 확인 필요.");
+        return;
+    }
 
-  var infowindow = new kakao.maps.InfoWindow({
-      content: '<div style="padding:5px;">은천제일교회</div>'
-  });
-  infowindow.open(map, marker);
+    var mapContainer = document.getElementById('map');
+    var mapOption = {
+        center: new kakao.maps.LatLng(37.486803, 126.937059),
+        level: 3
+    };
+
+    var map = new kakao.maps.Map(mapContainer, mapOption);
+
+    var markerPosition = new kakao.maps.LatLng(37.486803, 126.937059);
+    var marker = new kakao.maps.Marker({
+        position: markerPosition
+    });
+    marker.setMap(map);
+
+    var infowindow = new kakao.maps.InfoWindow({
+        content: '<div style="padding:5px;">은천제일교회</div>'
+    });
+    infowindow.open(map, marker);
 };
